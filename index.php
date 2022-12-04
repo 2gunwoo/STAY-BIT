edcafbc8d35590d909ad39669eb7d7fccae6e7ba
2gunwoo <106566831+2gunwoo@users.noreply.github.com>
2022-12-05 00:51:06 +0900

    STAY BIT 메인화면

---
+++ index.php
@@ -0,0 +1,182 @@
+<!DOCYPE html>
+
+<head>
+<meta charset="utf-8">
+<title>사이트</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<style>
+
+ body{
+		 background: linear-gradient(to right bottom, #6A5ACD, white 40%, #4B0082);
+             }
+pre{text-align:left; font-size:15px; color:black;}
+p{text-align:left;}
+</style>
+
+</head>
+
+<body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">STAY BIT : 암호화폐</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home(메인 이동 완료)</a>
+        </li>
+        <li class="nav-item">
+         <a class="nav-link active" aria-current="page" onclick="window.open('https://kr.tradingview.com/chart/?symbol=CRYPTOCAP%3AUSDT.D','window_name',  'width=500, height=600, scrollbars=yes');">테더 도미넌스</a>
+        </li>
+	<li class="nav-item">
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            테마
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="art.html">art</a></li>
+	<li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="vr_ar.html">vr/ar</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="web3.html">web3</a></li>
+	<li><hr class="dropdown-divider"></li>
+	<li><a class="dropdown-item" href="sports.html">sports</a></li>
+	<li><hr class="dropdown-divider"></li>
+	<li><a class="dropdown-item" href="memes.html">memes</a></li>
+	<li><hr class="dropdown-divider"></li>
+	<li><a class="dropdown-item" href="energy.html">energy</a></li>
+	<li><hr class="dropdown-divider"></li>
+	<li><a class="dropdown-item" href="soo.html">거래소 코인</a></li>
+          </ul>
+        </li>
+
+
+
+ <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            게시판&공지
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="dog.html">게시판</a></li>
+            <li><a class="dropdown-item" href="gong.html">공지사항</a></li>
+            <li><hr class="dropdown-divider"></li>
+	<li><a class="dropdown-item" href="up.html">업데이트 현황</a></li>
+          </ul>
+        </li>
+
+
+
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+	<li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="joo.html">관련 주</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+	<a href="file:///C:/Users/%EB%A0%88%EC%A7%80%EB%8D%B4%EC%85%9C/Desktop/login.html" button class="btn btn-outline-success" type="submit">Login</button></a>
+      </form>
+    </div>
+  </div>
+</nav>
+<br>
+<div class="container">
+  <div class="row">
+    <div class="col-lg-3">
+      <div class="card" style="width: 18rem;">
+  <img src="엘살바도르.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">엘살바도르</h5>
+    <p class="card-text">1대 법정화폐 2021년 09월 07일</p>
+    <a href="#" class="btn btn-primary">twitter</a>
+    <a href="#" class="btn btn-primary">웹사이트</a>
+  </div>
+</div>
+    </div>
+    <div class="col-lg-3">
+      <div class="card" style="width: 18rem;">
+  <img src="중앙아프리카공화국.png" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">중앙아프리카공화국</h5>
+    <p class="card-text">2대 법정화폐 2022년 04월 27일</p>
+    <a href="#" class="btn btn-primary">twitter</a>
+    <a href="#" class="btn btn-primary">웹사이트</a>
+  </div>
+</div>
+    </div>
+    <div class="col-lg-3">
+     <div class="card" style="width: 18rem;">
+  <img src="코스모스.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">3대 법정화폐 국가</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">twitter</a>
+    <a href="#" class="btn btn-primary">웹사이트</a>
+  </div>
+</div>
+    </div>
+    <div class="col-lg-3">
+      <div class="card" style="width: 18rem;">
+  <img src="코스모스.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">4대 법정화폐 국가</h5>
+    <p class="card-text"></p>
+    <a href="#" class="btn btn-primary">twitter</a>
+    <a href="#" class="btn btn-primary">웹사이트</a>
+  </div>
+</div>
+    </div>
+    <div class="col-lg-3">
+     <div class="card" style="width: 18rem;">
+  <img src="코스모스.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">일론머스크</h5>
+    <p class="card-text">테슬라, 스페이스X, 뉴럴링크 CEO</p>
+    <a href="https://twitter.com/elonmusk"class="btn btn-primary">twitter</a>
+   <a href="https://www.spacex.com/" class="btn btn-primary">스페이스X</a>
+  </div>
+</div>
+    </div>
+    <div class="col-lg-3">
+      <div class="card" style="width: 18rem;">
+  <img src="코스모스.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">마이클 세일러</h5>
+    <p class="card-text">MicroStrategy 공동 창업자</p>
+    <a href="https://twitter.com/saylor" class="btn btn-primary">twitter</a>
+    <a href="https://www.microstrategy.com/ko" class="btn btn-primary">MicroStrategy</a>
+  </div>
+</div>
+    </div>
+<div class="col-lg-3">
+      <div class="card" style="width: 18rem;">
+  <img src="코스모스.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">캐서린 우드</h5>
+    <p class="card-text">금융인, ARK invest 창업자</p>
+    <a href="https://twitter.com/cathiedwood"class="btn btn-primary">twitter</a>
+    <a href="https://ark-invest.com/"class="btn btn-primary">ARK invest</a>
+  </div>
+</div>
+    </div>
+<div class="col-lg-3">
+      <div class="card" style="width: 18rem;">
+  <img src="코스모스.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">창펑 자오</h5>
+    <p class="card-text">바이낸스 CEO, BNB코인 창시자</p>
+    <a href="https://twitter.com/cz_binance"class="btn btn-primary">twitter</a>
+    <a href="https://www.binance.com/en/futures/BNBUSDT" class="btn btn-primary">바이낸스</a>
+  </div>
+</div>
+    </div>
+  </div>
+</div><br>
+</body>
